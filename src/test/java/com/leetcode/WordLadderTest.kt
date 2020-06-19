3e6103ab1039de877360094465da15af3066017b
wezhyn <wezhyn@163.com>
2020-06-19 15:20:49 +0800

    WordLadder 06/19

---
+++ src/test/java/com/leetcode/WordLadderTest.kt
@@ -0,0 +1,23 @@
+package com.leetcode
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * @author wezhyn
+ * @since 06.19.2020
+ */
+class WordLadderTest {
+
+    @Test
+    fun ladderLength() {
+        assertEquals(3, WordLadder().ladderLength("hot", "dog", listOf("hot", "dog", "dot")))
+        assertEquals(2, WordLadder().ladderLength("a", "g", listOf("g")))
+        assertEquals(5, WordLadder().ladderLength("hit", "cog", listOf(
+                "hot", "dot", "dog", "lot", "log", "cog"
+        )))
+        assertEquals(0, WordLadder().ladderLength("hit", "cog", listOf(
+                "hot", "dot", "log", "cog"
+        )))
+    }
+}
