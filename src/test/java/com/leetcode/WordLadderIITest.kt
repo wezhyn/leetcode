445b0ab2b8496d1c99975455b320700cda727110
wezhyn <wezhyn@163.com>
2020-06-20 09:44:42 +0800

    WordLadderII 06/20

---
+++ src/test/java/com/leetcode/WordLadderIITest.kt
@@ -0,0 +1,26 @@
+package com.leetcode
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * @author wezhyn
+ * @since 06.19.2020
+ */
+class WordLadderIITest {
+
+    @Test
+    fun findLadders() {
+        assertEquals(listOf(listOf("red", "ted", "tad", "tax"), listOf("red", "ted", "tex", "tax"), listOf("red", "rex", "tex", "tax")),
+                WordLadderII().findLadders("red", "tax", listOf("ted", "tex", "red", "tax", "tad", "den", "rex", "pee")))
+        assertEquals(listOf(listOf("hit", "hot", "dot", "dog", "cog"), listOf("hit", "hot", "lot", "log", "cog")),
+                WordLadderII().findLadders("hit", "cog", listOf(
+                        "hot", "dot", "dog", "lot", "log", "cog"
+                )))
+        assertEquals(listOf(listOf("hot", "dot", "dog")), WordLadderII().findLadders("hot", "dog", listOf("hot", "dog", "dot")))
+        assertEquals(listOf(listOf("a", "g")), WordLadderII().findLadders("a", "g", listOf("g")))
+        assertEquals(listOf<List<String>>(), WordLadderII().findLadders("hit", "cog", listOf(
+                "hot", "dot", "log", "cog"
+        )))
+    }
+}
