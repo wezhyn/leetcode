8dfe554d27793757556895706e4d646f02349155
wezhyn <wezhyn@163.com>
2020-06-22 18:19:07 +0800

    LongestIncreasingPathInAMatrix 06/22

--- src/main/java/com/leetcode/graph/BusRoutes.kt
+++ src/main/java/com/leetcode/graph/BusRoutes.kt
@@ -1,5 +1,8 @@
 package com.leetcode.graph
 
+import com.DirectedEdgeGraph
+import com.Edge
+import com.get
 import java.util.*
 import kotlin.collections.ArrayList
 import kotlin.collections.HashMap
@@ -98,56 +101,4 @@ class BusRoutes {
         }
         return -1
     }
-}
-
-private class Edge<T>(val from: Int, val to: Int, val weight: T) : Comparable<Edge<T>>
-        where T : Comparable<T>, T : Number {
-
-    override fun toString(): String {
-        return "Edge(from=$from, to=$to, weight=$weight)"
-    }
-
-    fun isSelfConnect(): Boolean = from == to
-    override fun compareTo(other: Edge<T>): Int {
-        return weight.compareTo(other.weight)
-    }
-}
-
-private class DirectedEdgeGraph<T>(private val vertex: Int, edges: List<Edge<T>>)
-        where T : Comparable<T>, T : Number {
-
-    private val graph: Array<MutableList<Edge<T>>> = Array(vertex) { ArrayList<Edge<T>>() }
-
-    operator fun get(index: Int): Array<Edge<T>> = adjacency(index)
-
-    init {
-        for (edge in edges) {
-            graph[edge.from].add(edge)
-        }
-    }
-
-    fun v(): List<Int> {
-        val result = ArrayList<Int>()
-        for (i in graph.indices) {
-            result.add(i)
-        }
-        return result
-    }
-
-    fun adjacency(v: Int): Array<Edge<T>> {
-        return graph[v].toTypedArray()
-    }
-}
-
-private fun createIntEdge(hasWeight: Boolean = true, array: IntArray, defaultWeight: Int = 1): Edge<Int> {
-    val w = if (hasWeight) array[2] else defaultWeight
-    return Edge(array[0], array[1], w)
-}
-
-private fun createDirectEdgeGraph(twoArray: Array<IntArray>, vertex: Int, hasWeight: Boolean = true): DirectedEdgeGraph<Int> {
-    val result = ArrayList<Edge<Int>>()
-    twoArray.forEach {
-        result.add(createIntEdge(hasWeight = hasWeight, array = it))
-    }
-    return DirectedEdgeGraph(vertex, result)
 }
---
+++ src/main/java/com/leetcode/graph/LongestIncreasingPathInAMatrix.kt
@@ -0,0 +1,69 @@
+package com.leetcode.graph
+
+/**
+ *
+ *
+ * 一种节点上下左右走动的示例代码:
+ *
+ * offset= { 0，-1，0，1，0 }
+ *
+ * 从 i 开始， x + offset[ i ] ,y + offset[ i+1 ]
+ * 第一轮： (x,y-1)
+ * 第二轮： (x-1,y)
+ * 第三轮： (x,y+1)
+ * 第四轮： (x+1,y)
+ *
+ * 给定一个二维数组，求最大递增路径的长度
+ *
+ * @author wezhyn
+ * @since 06.22.2020
+ *
+ */
+class LongestIncreasingPathInAMatrix {
+
+    /**
+     * -1 代表还未开始
+     *
+     */
+    companion object {
+        val offset = intArrayOf(0, -1, 0, 1, 0)
+    }
+
+    private lateinit var dp: Array<IntArray>
+    fun longestIncreasingPath(matrix: Array<IntArray>): Int {
+        dp = Array(matrix.size) { IntArray(matrix[it].size) { -1 } }
+        var max = Int.MIN_VALUE
+        for ((i, arr) in dp.withIndex()) {
+            for ((j, v) in arr.withIndex())
+                if (v < 0) {
+                    max = Math.max(max, dfsPath(matrix, i, j))
+                }
+        }
+        return if (max == Int.MIN_VALUE) {
+            0
+        } else {
+            max
+        }
+    }
+
+    private fun dfsPath(matrix: Array<IntArray>, i: Int, j: Int): Int {
+
+        if (dp[i][j] > 0) {
+            return dp[i][j]
+        }
+        var max = 1
+        for (of in 0..3) {
+            val xi = i + offset[of]
+            val yi = j + offset[of + 1]
+            if (xi < 0 || yi < 0 || xi >= matrix.size || yi >= matrix[xi].size) {
+                continue
+            }
+            if (matrix[i][j] < matrix[xi][yi]) {
+                max = Math.max(max, 1 + dfsPath(matrix, xi, yi))
+            }
+        }
+        dp[i][j] = max
+        return max
+    }
+
+}
