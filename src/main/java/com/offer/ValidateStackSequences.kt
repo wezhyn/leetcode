572151a1f25b46ad736f173e23dd86a8b9939864
wezhyn <wezhyn@163.com>
2020-06-30 16:11:37 +0800

    ValidateStackSequences.kt 6/30

---
+++ src/main/java/com/offer/ValidateStackSequences.kt
@@ -0,0 +1,38 @@
+package com.offer
+
+import java.util.*
+
+/**
+ *
+ * @author wezhyn
+ * @since 06.30.2020
+ *
+ */
+class ValidateStackSequences {
+    fun validateStackSequences(pushed: IntArray, popped: IntArray): Boolean {
+        if (pushed.isEmpty() && popped.isEmpty()) {
+            return true
+        } else if (pushed.isEmpty() || popped.isEmpty()) {
+            return false
+        }
+        val stack = LinkedList<Int>()
+        var popIndex = 0
+        var pushIndex = 0
+        while (popIndex < popped.size && pushIndex <= pushed.size) {
+            val pop = popped[popIndex]
+            if (pop != stack.peek()) {
+                if (pushIndex >= pushed.size) {
+                    break
+                }
+                stack.push(pushed[pushIndex++])
+            } else {
+                stack.poll()
+                popIndex++
+            }
+        }
+        if (popIndex < popped.size) {
+            return false
+        }
+        return true
+    }
+}
