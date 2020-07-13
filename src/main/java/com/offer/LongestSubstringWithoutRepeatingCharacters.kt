bd3829be350de1d1e8fcffc9ee8351c2b38b3e95
wezhyn <wezhyn@163.com>
2020-07-13 17:14:55 +0800

    LongestSubstringWithoutRepeatingCharacters.kt 7/13

---
+++ src/main/java/com/offer/LongestSubstringWithoutRepeatingCharacters.kt
@@ -0,0 +1,27 @@
+package com.offer
+
+/**
+ *
+ * @author wezhyn
+ * @since 07.12.2020
+ *
+ */
+class LongestSubstringWithoutRepeatingCharacters {
+    fun lengthOfLongestSubstring(s: String): Int {
+        val map = HashMap<Char, Int>()
+        var max = 0
+        var cur = 0
+        for ((i, c) in s.withIndex()) {
+            val preIndex = map[c] ?: -1
+            if (i - preIndex > cur) {
+                cur++
+            } else {
+                max = Math.max(cur, max)
+                cur = i - preIndex
+            }
+            map[c] = i
+        }
+        max = Math.max(cur, max)
+        return max
+    }
+}
