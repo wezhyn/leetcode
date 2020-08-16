bcd10a81bd50a53c5c2eabab5ae2c117366c7fd7
wezhyn <wezhyn@163.com>
2020-08-16 08:21:47 +0800

    携程 8/15

---
+++ src/test/java/com/code/meituan/ReversePairTest.java
@@ -0,0 +1,15 @@
+package com.code.meituan;
+
+import org.junit.Test;
+
+/**
+ * @author wezhyn
+ * @since 08.15.2020
+ */
+public class ReversePairTest {
+
+    @Test
+    public void reverse() {
+        System.out.println(ReversePair.reverse(1234));
+    }
+}
