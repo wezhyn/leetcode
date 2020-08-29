b0cf5ee7bd78ceeb9e0451c96fb6bb52671a6b8c
wezhyn <wezhyn@163.com>
2020-08-29 21:03:05 +0800

    众安  8/29

---
+++ src/test/java/com/code/zhongan/LongestRepeatNumTest.java
@@ -0,0 +1,19 @@
+package com.code.zhongan;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author wezhyn
+ * @since 08.29.2020
+ */
+public class LongestRepeatNumTest {
+
+    @Test
+    public void longestRepeatingSubstring() {
+        assertEquals(2, new LongestRepeatNum().longestRepeatingSubstring("abbadc"));
+        assertEquals(2, new LongestRepeatNum().longestRepeatingSubstring("abb"));
+        assertEquals(0, new LongestRepeatNum().longestRepeatingSubstring("abs"));
+    }
+}
---
+++ src/test/java/com/leetcode/top/interview/easy/MergeTwoSortedListsTest.java
@@ -0,0 +1,17 @@
+package com.leetcode.top.interview.easy;
+
+import com.leetcode.linked.ListNode;
+import com.leetcode.linked.MergeTwoSortedLists;
+import org.junit.Test;
+
+/**
+ * @author wezhyn
+ * @since 08.29.2020
+ */
+public class MergeTwoSortedListsTest {
+
+    @Test
+    public void mergeTwoLists() {
+        new MergeTwoSortedLists().mergeTwoLists(null, new ListNode(0));
+    }
+}
