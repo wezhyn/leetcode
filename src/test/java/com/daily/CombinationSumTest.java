ecb293f3eadc1162569651bd4c0cf7e395e7fb2a
wezhyn <nyhzew@gmail.com>
2020-09-09 20:38:25 +0800

    leetcode 9/9

---
+++ src/test/java/com/daily/CombinationSumTest.java
@@ -0,0 +1,18 @@
+package com.daily;
+
+import org.junit.Test;
+
+/**
+ * @author wezhyn
+ * @since 09.09.2020
+ */
+public class CombinationSumTest {
+
+    @Test
+    public void combinationSum() {
+
+        System.out.println(new CombinationSum().combinationSum(
+                new int[]{2, 3, 6, 7}, 7
+        ));
+    }
+}
